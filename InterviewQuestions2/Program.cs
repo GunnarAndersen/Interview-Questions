c492dfb4d6a7ec18cf2fb5bf29f7bd3e40f764f4
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-11-04 16:38:26 -0700

    Add files via upload

---
+++ InterviewQuestions2/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myInts = new List<int>();
+            int sum = myInts.Sum();
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
